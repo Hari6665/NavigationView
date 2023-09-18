c3453973ce10785a5432f96ae45cac937e179aab
Harikrishnan K M <harikunjath407@gmail.com>
2023-09-18 14:46:09 +0530

    Initial Commit

---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by Harikrishnan K M on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
